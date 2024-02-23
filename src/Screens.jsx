08f2d0c1d7c93359d9d06e10d831f93485794246
mikecasey12 <ikebudemichael@gmail.com>
2024-02-23 05:14:25 +0100

    final build

---
+++ src/Screens.jsx
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import confetti from "canvas-confetti";
+import * as icons from "react-icons/gi";
+import { Tile } from "./Tile";
+
+export const possibleTileContents = [
+  icons.GiHearts,
+  icons.GiWaterDrop,
+  icons.GiDiceSixFacesFive,
+  icons.GiUmbrella,
+  icons.GiCube,
+  icons.GiBeachBall,
+  icons.GiDragonfly,
+  icons.GiHummingbird,
+  icons.GiFlowerEmblem,
+  icons.GiOpenBook,
+];
+
+export function StartScreen({ start }) {
+  return (
+    <div className="h-screen w-full flex items-center justify-center px-8">
+      <div className="bg-pink-100 rounded-xl px-12 py-16 flex flex-col items-center justify-center text-pink-500 gap-10">
+        <h1 className="font-bold text-3xl">Memory</h1>
+        <p className="font-medium">Flip over tiles looking for pairs</p>
+        <button
+          onClick={start}
+          className="rounded-full w-[160px] px-1 py-2 bg-gradient-to-b from-pink-400 to-pink-600 text-white p-3"
+        >
+          Play
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export function PlayScreen({ end }) {
+  const [tiles, setTiles] = useState(null);
+  const [tryCount, setTryCount] = useState(0);
+
+  const getTiles = (tileCount) => {
+    // Throw error if count is not even.
+    if (tileCount % 2 !== 0) {
+      throw new Error("The number of tiles must be even.");
+    }
+
+    // Use the existing list if it exists.
+    if (tiles) return tiles;
+
+    const pairCount = tileCount / 2;
+
+    // Take only the items we need from the list of possibilities.
+    const usedTileContents = possibleTileContents.slice(0, pairCount);
+
+    // Double the array and shuffle it.
+    const shuffledContents = usedTileContents
+      .concat(usedTileContents)
+      .sort(() => Math.random() - 0.5)
+      .map((content) => ({ content, state: "start" }));
+
+    setTiles(shuffledContents);
+    return shuffledContents;
+  };
+
+  const flip = (i) => {
+    // Is the tile already flipped? We don’t allow flipping it back.
+    if (tiles[i].state === "flipped") return;
+
+    // How many tiles are currently flipped?
+    const flippedTiles = tiles.filter((tile) => tile.state === "flipped");
+    const flippedCount = flippedTiles.length;
+
+    // Don't allow more than 2 tiles to be flipped at once.
+    if (flippedCount === 2) return;
+
+    // On the second flip, check if the tiles match.
+    if (flippedCount === 1) {
+      setTryCount((c) => c + 1);
+
+      const alreadyFlippedTile = flippedTiles[0];
+      const justFlippedTile = tiles[i];
+
+      let newState = "start";
+
+      if (alreadyFlippedTile.content === justFlippedTile.content) {
+        confetti({
+          ticks: 100,
+        });
+        newState = "matched";
+      }
+
+      // After a delay, either flip the tiles back or mark them as matched.
+      setTimeout(() => {
+        setTiles((prevTiles) => {
+          const newTiles = prevTiles.map((tile) => ({
+            ...tile,
+            state: tile.state === "flipped" ? newState : tile.state,
+          }));
+
+          // If all tiles are matched, the game is over.
+          if (newTiles.every((tile) => tile.state === "matched")) {
+            setTimeout(end, 0);
+          }
+
+          return newTiles;
+        });
+      }, 1000);
+    }
+
+    setTiles((prevTiles) => {
+      return prevTiles.map((tile, index) => ({
+        ...tile,
+        state: i === index ? "flipped" : tile.state,
+      }));
+    });
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center p-8 h-screen w-full gap-10">
+        <h3 className="font-medium text-lg text-indigo-500">
+          Tries:{" "}
+          <span className="bg-indigo-300 px-2 py-1 rounded-md">
+            {tryCount}
+          </span>
+        </h3>
+        <div className="bg-indigo-100 rounded-xl grid grid-cols-4 gap-4 p-4">
+          {getTiles(16).map((tile, i) => (
+            <Tile key={i} flip={() => flip(i)} {...tile} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/Tile.jsx
@@ -0,0 +1,51 @@
+export function Tile({ content: Content, flip, state }) {
+  switch (state) {
+    case "start":
+      return (
+        <Back
+          className="inline-block h-16 w-16 bg-indigo-300 rounded-lg text-center"
+          flip={flip}
+        />
+      );
+    case "flipped":
+      return (
+        <Front className="inline-block h-16 w-16 bg-indigo-500 text-white p-2 rounded-lg">
+          <Content
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100%",
+              verticalAlign: "top",
+            }}
+          />
+        </Front>
+      );
+    case "matched":
+      return (
+        <Matched className="inline-block h-16 w-16 text-indigo-200">
+          <Content
+            style={{
+              display: "inline-block",
+              width: "100%",
+              height: "100%",
+              verticalAlign: "top",
+            }}
+          />
+        </Matched>
+      );
+    default:
+      throw new Error("Invalid state " + state);
+  }
+}
+
+function Back({ className, flip }) {
+  return <div onClick={flip} className={className}></div>;
+}
+
+function Front({ className, children }) {
+  return <div className={className}>{children}</div>;
+}
+
+function Matched({ className, children }) {
+  return <div className={className}>{children}</div>;
+}
